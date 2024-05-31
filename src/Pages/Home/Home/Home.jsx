4f9b9162b2caecaadfe8a0ef3c0e26eb3020ad4a
Md Ibrahim <mdibrahim36194@gmail.com>
2024-05-31 15:32:09 +0600

    project setup and toNav added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import TopNav from "../../../Shared/TopNav/TopNav";
+
+const Home = () => {
+
+    // #f98821
+    // #61bd12
+    return (
+        <div>
+            <TopNav></TopNav>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Shared/TopNav/TopNav.jsx
@@ -0,0 +1,18 @@
+
+const TopNav = () => {
+    return (
+        <div className="bg-[#61bd12] p-2 text-center text-white">
+            <div className="flex justify-between w-[95%] mx-auto">
+                <div>
+                    <p>Tech care of you and your families with SI Organic Bazaar</p>
+                </div>
+                <div>
+                    <span className="me-5">+8801996-233134</span>
+                    <span>+8801956-633205</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopNav;
